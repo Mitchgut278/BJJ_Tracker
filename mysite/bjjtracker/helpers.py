03bae25fd0e992d58e7b170c47fde6249169c9d2
Mitchgut278 <gutmanmitchell@gmail.com>
2022-08-23 17:54:19 -0400

    fix motd streak

--- mysite/bjjtracker/helpers.py
+++ mysite/bjjtracker/helpers.py
@@ -13,10 +13,13 @@ def getCurrentStreak(request):
     for date in dates:
         date = date['date']
         delta = compareDate - date
+
         if delta.days == 1: # Keep the streak going!
             current_streak += 1
+            compareDate = date
         elif delta.days == 0: # Don't bother increasing the day if there's multiple ones on the same day
             pass
         else: 
             break 
+
     return current_streak
